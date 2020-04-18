5bf07f57c2d53bf88c5c041a2c14e79d7c2ce5da
Dwicandra <dwicandra.ekastrya@gmail.com>
2020-04-19 01:10:06 +0700

    Add seeder for users table

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $superAdmin				= new User();
+        $superAdmin->password 	= bcrypt('password');
+        $superAdmin->email		= 'admin@travelspot.id';
+        $superAdmin->name		= 'Agus Ma\'ruf';
+        $superAdmin->save();
+    }
+}
