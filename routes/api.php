017ccc3361a6f0b7537e92592a7b46593cdc757c
Dwicandra <dwicandra.ekastrya@gmail.com>
2021-03-04 07:41:07 +0700

    API route to convert currencies

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/moneyapp/usd-to-idr',  'MoneyApp\CurrencyLogController');
+Route::get('/convert',  'MoneyApp\CurrencyController@getConvert');
